b32f921cb1032edbca64e2cc0a3ee27907b70148
Bülent TÜRÜDÜ <120729324+bulentturudu@users.noreply.github.com>
2025-08-20 14:46:42 +0300

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Bülent TÜRÜDÜ on 20.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
